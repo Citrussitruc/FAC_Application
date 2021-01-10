7ca14be5023a709b24f2a0c37b520d7d1580f325
Curtis <curtis.c.turk@outlook.com>
2021-01-10 11:48:46 +0000

    Anagram hover script

--- scripts.js
+++ scripts.js
@@ -41,15 +41,26 @@ prevBtn.addEventListener('click',()=>{
 carouselSlide.addEventListener('transitionend',()=>{
     
     if(carouselImages[counter].id === 'lastClone'){
-        console.log("This is returning to the end");
         carouselSlide.style.transition = "none";
         counter = carouselImages.length - 2;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
     if(carouselImages[counter].id === 'firstClone'){
-        console.log("This is returning to the beginning")
         carouselSlide.style.transition = "none";
         counter = carouselImages.length - counter;
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 });
+
+const citrus = document.querySelector('#Citrus h1 a');
+let CitrusCurtis = 'Citrus'
+function anagram(){
+    if(CitrusCurtis == 'Citrus'){
+        citrus.innerHTML = CitrusCurtis
+        CitrusCurtis = 'Curtis'
+    }else if(CitrusCurtis == 'Curtis'){
+        citrus.innerHTML = CitrusCurtis
+        CitrusCurtis = 'Citrus'
+    }
+    
+}
